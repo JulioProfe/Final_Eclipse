726dbbee112e76b6b761fce7338bb29468d9d091
IvanDario <idacera@hotmail.com>
2017-05-15 15:33:38 -0500

    holitos

---
+++ src/Logica.java
@@ -0,0 +1,8 @@
+
+public class Logica {
+
+	public Logica(Main main) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import processing.core.PApplet;
 
-public class Main {
+public class Main extends PApplet {
+	
+	private Logica log;
 
 	public static void main(String[] args) {
+		PApplet.main("Main");
+
+	}
+	
+	public void settings() {
+		size(1200,700);
 		// TODO Auto-generated method stub
 
 	}
+	
+	@Override
+	public void setup() {
+		log = new Logica(this);
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void draw() {
+		background(255);
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
